90e2b409061e186e66abd902561195fb6f40827a
Siri <ssiri3731@gmail.com>
2026-08-10 16:59:30 +0530

    Add files via upload

--- short-circutin operator.js
+++ short-circutin operator.js
@@ -1,3 +1,10 @@
-//1 Logical &&
-let result = false && "Hello";
-console.log(result);
+//1 Logical  and &&
+// let result = false && "Hello";
+// console.log(result);
+
+//2.logical OR || 
+// let result = false ||"Hello";
+// console.log(result);
+//3.Nullish coalescing ??
+let username = null ?? "Guest";
+console.log(username);
